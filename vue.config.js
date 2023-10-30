705a08b0a9f5a30c08a151822a7094717c2a2bd9
joren97x <babamyloves2002@gmail.com>
2023-10-30 20:10:05 +0630

    PINAKA LAST NGA COMMIT

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/weight-tracker/',
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
